56bb8eeb32ee707b6996df8754af755a3a20f534
ProgramistycznySwir <39069042+ProgramistycznySwir@users.noreply.github.com>
2018-11-09 17:55:01 +0100

    version 0.0.9.3b
    small fix and log screen 1.0 is ready, now i have only to work on aesthetics, like not dividing the text

--- Graphics.cs
+++ Graphics.cs
@@ -279,31 +279,36 @@ namespace RougeMechsGraphics
             Draw.Frame(ULCornerPosition, size);
 
             //Write("LogSystem: Log screen size: " + size.x + " ; " + size.y);
+            Write("**********************************************************************************************************************************************************************************************");
+            Write("It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midi-chlorians to create life. ... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying.");
             Write("Mark Edward Fischbach (born June 28, 1989),[1] known online as Markiplier, (or simply Mark), is an American YouTuber. Originally from Honolulu, Hawaii,[2] he began his career in Cincinnati, Ohio, and is currently based in Los Angeles, California.[3]As of August 2018, his channel has over 9 billion total video views and 22 million subscribers,[4][5] and is currently the 36th most - subscribed channel on YouTube.[6] Fischbach specializes in Let's Play videos, commonly of survival horror video games.[7]");
         }
         public static void Write(string text)
         {
             
-            if(text.Length > size.x - 1)
+            if(text.Length > size.x - 2)
             {
                 //IEnumerable<char> text2 = text.Take(size.x - 1).ToArray();
-                Char[] text2 = text.Take(size.x - 2).ToArray();
+                Char[] text2 = text.Take(size.x - 1).ToArray();
                 int iteration = 0;
-                while (text2.Length >= size.x - 2)
+                while (text2.Length >= size.x - 1)
                 {
                     iteration++;
                     QoL.GotoXY(ULCornerPosition.x + 1, lastLogLine);
                     Console.Write(text2);
-                    text2 = text.Skip((size.x - 3) * iteration).Take(size.x - 1).ToArray();
+                    text2 = text.Skip((size.x - 1) * iteration).Take(size.x - 1).ToArray();
                     lastLogLine++;
                 }
-                //Console.Write(text3);
+                QoL.GotoXY(ULCornerPosition.x + 1, lastLogLine);
+                text2 = text.Skip((size.x - 1) * iteration).Take(size.x - 1).ToArray();
+                Console.Write(text2);
             }
             else
             {
                 QoL.GotoXY(ULCornerPosition.x + 1, lastLogLine);
                 Console.Write(text);
-            }            
+            }
+            lastLogLine++;
         }
     }
 }
